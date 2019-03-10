3e38f8473bb1e205d037271d5f5248b648ae1a30
martink-io <mkehayov92@gmail.com>
2019-03-10 22:46:46 +0000

    Display service hours

--- app/models/user_product_service.rb
+++ app/models/user_product_service.rb
@@ -1,8 +1,11 @@
 class UserProductService < UserProduct
   before_save :calculate_cost
 
-  def calculate_cost
+  def hours
     hours = (end_time - start_time) / 3600
+  end
+
+  def calculate_cost
     self.cost = hours * qty * product.unit_price
   end
 end
