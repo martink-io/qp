fbc1e82ca27e55ec6017980b5a05bd2e8be2f29f
martink-io <mkehayov92@gmail.com>
2019-03-10 20:41:47 +0000

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(name: "RoR course", unit_price: 12.50, type: "Subscription")
-Product.create(name: "DevOps course", unit_price: 10.00, type: "Subscription")
-Product.create(name: "ReactJS course", unit_price: 8.00, type: "Subscription")
+Product.create(name: "RoR course", unit: 'day', unit_price: 12.50, type: "Subscription")
+Product.create(name: "DevOps course", unit: 'day', unit_price: 10.00, type: "Subscription")
+Product.create(name: "ReactJS course", unit: 'day', unit_price: 8.00, type: "Subscription")
 
-Product.create(name: "RoR tricks", unit_price: 2.50, type: "Service")
-Product.create(name: "DevOps for AWS", unit_price: 5.99, type: "Service")
-Product.create(name: "Redux approach", unit_price: 4.75, type: "Service")
+Product.create(name: "RoR tricks", unit: 'hour', unit_price: 2.50, type: "Service")
+Product.create(name: "DevOps for AWS", unit: 'hour', unit_price: 5.99, type: "Service")
+Product.create(name: "Redux approach", unit: 'hour', unit_price: 4.75, type: "Service")
 
 Product.create(name: "RoR by MIT", unit_price: 8.99, type: "Good")
 Product.create(name: "ReactJS by MIT", unit_price: 5.99, type: "Good")
