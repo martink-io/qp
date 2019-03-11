5134634772426c51d18576603f7a836784ff91d8
martink-io <mkehayov92@gmail.com>
2019-03-11 08:53:37 +0000

    Refactor validations for UserProductService

--- app/models/user_product.rb
+++ app/models/user_product.rb
@@ -1,16 +1,7 @@
 class UserProduct < ApplicationRecord
-  ALLOWED_DAYS = ['Monday',
-                  'Tuesday',
-                  'Wednesday',
-                  'Thursday',
-                  'Friday',
-                  'Saturday'].freeze
-
   belongs_to :product
-  
+
   validates_presence_of :product_id, :type
   validates :cost, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, 
     numericality: { greater_than: 0 }
-  validates :recurring_on
-  validates_inclusion_of :recurring_on, in: ALLOWED_DAYS
 end
--- app/models/user_product_service.rb
+++ app/models/user_product_service.rb
@@ -1,20 +1,20 @@
 class UserProductService < UserProduct
-  validates_presence_of :qty, :recurring_on, :start_time, :end_time
+  ALLOWED_DAYS = ['Monday',
+                  'Tuesday',
+                  'Wednesday',
+                  'Thursday',
+                  'Friday',
+                  'Saturday'].freeze
 
+  validates_presence_of :qty, :recurring_on, :start_time, :end_time
   validates_datetime :end_time, :after => :start_time 
   validates_time :start_time, :on_or_after => '9:00am', 
     :on_or_after_message => 'must be at/after 9am'
   validates_time :end_time, :on_or_before => '7:00pm',
     :before_message => 'must be at/before 7pm'
-  
-  validate :mon_to_sat
+  validates_inclusion_of :recurring_on, in: ALLOWED_DAYS
 
   before_save :calculate_cost
-
-  def mon_to_sat
-    return unless recurring_on == 'Sunday'
-    errors[:recurring_on] << "We do not provide services on Sunday"
-  end
 
   def hours
     hours = (end_time - start_time) / 3600
